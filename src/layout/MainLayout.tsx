857cb7d5eadd1ab18c4785ac546e787e16df162d
seyunkims <rtd0203@gmail.com>
2026-02-10 15:33:40 +0900

    feat:상단에서 시작

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,17 +1,11 @@
-// src/layout/MainLayout.tsx
-
-/**
- * 공통 레이아웃 컴포넌트
- *
- * props 대신 Zustand 스토어에서 직접 user 정보를 가져옵니다.
- */
-
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
 
 function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
 
       <main className="flex-1 container-main py-8">
@@ -20,7 +14,7 @@ function Layout() {
 
       <footer className="border-t">
         <div className="container-main py-4 text-center text-sm">
-          © 2026 My Dev Blog. Built with React + Firebase
+          © 2025 My Dev Blog. Built with React + Firebase
         </div>
       </footer>
     </div>
